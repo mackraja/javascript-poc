4569ebe5ead1308078061e21c74945c26f5bbf33
mackraja <montykhanna007@hotmail.com>
2021-12-23 21:56:45 +0530

    :zap:

--- prog/crazy.js
+++ prog/crazy.js
@@ -21,6 +21,20 @@ console.log(firstCharCapitalize('monty')); // Monty
 [...Array(10).keys()].sort(() => 0.5 - Math.random())
 
 
+// Fastest way to removes duplicate numbers of array
+const arr = Array.from(new Set([2,3,4,2,3,5,6,8,9,8]));
+// OR
+const arr = [...new Set([2,3,4,2,3,5,6,8,9,8])];
+console.log(arr);
+
+
+// Easiest way to remove duplicate objects from an Array
+const keyValue = (x) => ([JSON.stringify(x), x]);
+const unique = (x) => [...new Map(x.map(keyValue)).values()];
+const arr = [{a:1}, {b:2}, {a:1}];
+console.log(unique(arr));
+
+
 // Create emoji characters from a numeric value
 const smile = String.fromCodePoint(0x1F600);
 const str = `${smile} Please`
@@ -29,3 +43,9 @@ console.log(str);
 
 // More Gist from Nick
 https://gist.github.com/codemile
+
+// Javascript Cheat Sheet
+https://files.suhailkakar.com/javascript.png
+
+// More about this keyword in javascript
+https://faheemkhan97.hashnode.dev/the-mysterious-this-keyword-lets-demystify-it
